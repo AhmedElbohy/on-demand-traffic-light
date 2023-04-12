5ce7f5fb7f148d5d6c2fd3ea2104a4cee0501948
Ahmed Elbohy <127188037+AhmedElbohy@users.noreply.github.com>
2023-04-12 13:31:11 +0200

    Update main.c

--- on-demand traffic light/project2/main.c
+++ on-demand traffic light/project2/main.c
@@ -7,22 +7,15 @@
 
 #define F_CPU 8000000UL
 #include <util/delay.h>
-#include "APP/application.h"
+#include "APP/APP.h"
 
 
 /**/int main(void)
 {
-	App_Init(PORTA);
+	APP_INIT();
 	while (1)
 	{
-		//Flaching(PORTA);
-		//Shift_Left(PORTA);
-		//Shift_Right(PORTA);
-		//Converging(PORTA);
-		//Diverging(PORTA);
-		//Pingpong(PORTA);
-		//Snake(PORTA);
-		Converging_Diverging(PORTA);
+		APP_Start();
 	}
 	
 }
